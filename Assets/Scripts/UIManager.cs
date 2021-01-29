040c23e9a4d9f24a5c7df58dd2776763d4576a30
Jakobi Talbot <jakobi.talbot@gmail.com>
2021-01-29 21:34:51 +1100

    UI Manager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    [SerializeField]
+    int numberOfCollectables;
+    [SerializeField]
+    TextMeshProUGUI time;
+    [SerializeField]
+    TextMeshProUGUI progress;
+
+    float secondsSinceStart;
+    int numberCollected;
+
+    void Start()
+    {
+        if (!instance)
+            instance = this;
+        StartCoroutine(Count());
+    }
+
+    IEnumerator Count()
+    {
+        while (true)
+        {
+            secondsSinceStart += Time.deltaTime;
+            int minutes = (int)(secondsSinceStart / 60f);
+            int seconds = (int)(secondsSinceStart % 60f);
+            time.text = minutes + ":" + (seconds < 10 ? "0" + seconds.ToString() : seconds.ToString());
+
+            yield return null;
+        }
+    }
+
+    public void IncrementProgress()
+    {
+        ++numberCollected;
+        progress.text = numberCollected + "/" + numberOfCollectables;
+    }
+}
